94efcf20d05cb17b9f5f882ea6fa6934554d5be6
KevinWang12138 <wangqianming12138@gmail.com>
2022-07-23 14:10:04 +0800

    首页

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"GoBlog/config"
+	"GoBlog/models"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 /**
@@ -15,13 +19,67 @@ type IndexData struct {
 	Desc  string `json:"desc"`
 }
 
+func isODD(num int) bool {
+	return num%2 == 0
+}
+
+func getNextName(strs []string, index int) string {
+	return strs[index+1]
+}
+
+func date(str string) string {
+	return time.Now().Format(str)
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json") //设置返回的数据是Json格式
-	var indexData IndexData
-	indexData.Title = "GoBlog"
-	indexData.Desc = "这是一个Go语言开发的博客"
-	jsonStr, _ := json.Marshal(indexData)
-	w.Write(jsonStr)
+	//拿到当前路径
+	path, _ := os.Getwd()
+	t := template.New("index.html")
+	//因为访问页面的时候有多个模板嵌套，因此需要把需要的数据全部解析
+	home := path + "/template/home.html"
+	header := path + "/template/layout/header.html"
+	footer := path + "/template/layout/footer.html"
+	personal := path + "/template/layout/personal.html"
+	post := path + "/template/layout/post-list.html"
+	pagination := path + "/template/layout/pagination.html"
+	//解析文件
+	t.Funcs(template.FuncMap{"isODD": isODD, "getNextName": getNextName, "date": date})
+	t, err := t.ParseFiles(path+"/template/index.html", home, header, footer, personal, post, pagination)
+
+	if err != nil {
+		log.Println("解析模板出现错误", err)
+	}
+	//页面上涉及的数据必须有定义
+	var categorys = []models.Category{
+		{
+			Cid:  1,
+			Name: "go",
+		},
+	}
+	var posts = []models.PostMore{
+		{
+			Pid:          1,
+			Title:        "go博客",
+			Content:      "内容",
+			UserName:     "码神",
+			ViewCount:    123,
+			CreateAt:     "2022-02-20",
+			CategoryId:   1,
+			CategoryName: "go",
+			Type:         0,
+		},
+	}
+	var hr = &models.HomeResponse{
+		config.Cfg.Viewer,
+		categorys,
+		posts,
+		1,
+		1,
+		[]int{1},
+		true,
+	}
+	//返回页面
+	t.Execute(w, hr)
 }
 
 func main() {
@@ -31,6 +89,7 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/", index)
+	http.Handle("/resource/", http.StripPrefix("/resource/", http.FileServer(http.Dir("public/resource/"))))
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
